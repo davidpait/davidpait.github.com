8587cf2bbba7d1ae6d89ecac23e1ce8cc8388ff5
David Pait <pait.david@gmail.com>
2012-11-11 23:39:38 -0500

    updated css and js

--- js/fun.js
+++ js/fun.js
@@ -3,23 +3,23 @@ $('div.span12.name').mouseover(function() {
   var lname = $('.last'),
       fname = $('.first');
 
-  if (lname.css('margin-top') == '30px') {
+  if (lname.css('margin-top') == '0px') {
       lname.animate({
-        'margin-top' : '+=30',
+        'margin-top' : '30px',
         'color' : '#ccc',
-        'margin-left' : 60
+        'margin-left' : '60px'
       });
       fname.animate({
-      'margin-left':40
+      'margin-left': '40px'
       });
     } else {
        lname.animate({
-        'margin-top' : '0',
-         'margin-left' : 100
+        'margin-top' : '0px',
+         'margin-left' : '100px'
         
       });
       fname.animate({
-        'margin-left': 0
+        'margin-left': '0px'
       });
     }
 });
