7ff0a3559ee1b221ac9041a3fc755ecea40311cd
David Pait <pait.david@gmail.com>
2012-11-11 23:19:12 -0500

    fixed animation margins

--- js/fun.js
+++ js/fun.js
@@ -1,18 +1,19 @@
 $('div.span12.name').hover(function() {
   $('.last').animate({
     'margin-top' : '+=30',
-    'color' : '#ccc'
+    'color' : '#ccc',
+    'margin-left' : 60
   });
   $('.first').animate({
-  'margin-left':80
+  'margin-left':40
   });
 }, function() {
    $('.last').animate({
-    'margin-top' : '0px',
-     left : 0
+    'margin-top' : '0',
+     'margin-left' : 100
     
   });
   $('.first').animate({
-    'margin-left':0
+    'margin-left': 0
   });
 });
